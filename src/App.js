f506642a9634cfc61ed60c083e1bf54c6a23a50b
Nanaya <nanayamiki@gmail.com>
2023-07-11 19:16:44 -0700

    complete the tasks and the app

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import AngryButton from "./components/AngryButton";
+import CounterButton from "./components/CounterButton";
+import LightSwitchButton from "./components/LightSwitchButton";
+import TextRepeaterButton from "./components/TextRepeaterButton";
+import { useState } from "react";
+
+function App() {
+	const [light, setLight] = useState(false);
+	const dark = light ? " " : "dark";
+
+	const switchLight = () => {
+		setLight((prevState) => !prevState);
+	};
+
+	return (
+		<div className={`App ${dark}`}>
+			<h1>Fancy Buttons!</h1>
+			<section>
+				<AngryButton />
+				<CounterButton />
+				<LightSwitchButton light={light} handleClick={switchLight} />
+				<TextRepeaterButton />
+			</section>
+		</div>
+	);
+}
+
+export default App;
