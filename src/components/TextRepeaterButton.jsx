bbd9697957e5db1647982a24c8552ab3076a2c7d
Nanaya <nanayamiki@gmail.com>
2023-08-13 13:22:55 -0700

    second time creating components

--- src/components/TextRepeaterButton.jsx
+++ src/components/TextRepeaterButton.jsx
@@ -3,15 +3,16 @@ import { useState } from "react";
 
 function TextRepeaterButton() {
 	const [repetitions, setRepetitions] = useState(1);
-	const textArray = [];
-	const handleClick = () => {
-		setRepetitions(repetitions + 1);
+	const clickHandler = () => {
+		setRepetitions((prev) => prev + 1);
 	};
+	const textArray = [];
 	for (let i = 0; i < repetitions; i++) {
 		textArray.push(<span key={i}>I like this text</span>);
 	}
+
 	return (
-		<button className="TextRepeaterButton" onClick={handleClick}>
+		<button className="TextRepeaterButton" onClick={clickHandler}>
 			{textArray}
 		</button>
 	);
