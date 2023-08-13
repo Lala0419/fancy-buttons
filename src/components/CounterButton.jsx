3baa81284e3edd1b2c059bcde5920ebaf004b02e
Nanaya <nanayamiki@gmail.com>
2023-08-13 13:22:46 -0700

    second time creating components

--- src/components/CounterButton.jsx
+++ src/components/CounterButton.jsx
@@ -3,11 +3,11 @@ import { useState } from "react";
 
 function CounterButton() {
 	const [clickAmount, setClickAmount] = useState(0);
-	const handleClick = () => {
-		setClickAmount(clickAmount + 1);
+	const clickHandler = () => {
+		setClickAmount((prev) => prev + 1);
 	};
 	return (
-		<button className="CounterButton" onClick={handleClick}>
+		<button className="CounterButton" onClick={clickHandler}>
 			You clicked me {clickAmount} amount of times
 		</button>
 	);
